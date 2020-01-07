e2754c7c79aa6516bbe8087ff548ab9ab3406c8c
jordanrob709 <jordanrob709@gmail.com>
2020-01-07 23:38:38 +0300

    created the first view

--- ToDo/models.py
+++ ToDo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+"""class Users(models.Model):
+    Full_names = models.CharField(max_length=200)
+    Address = models.CharField(max_length=200)
+"""
+
+
+"""class Todo(models.Model):
+    description = models.CharField(max_length=200)
+    Todo_date = models.DateTimeField('Todo Date')
+
+    def __str__(self):
+        return self.description
+
+    def create(self, description, Todo_date):
+        todo = Todo(description=description, Todo_date=Todo_date)
+        todo.save()
+        return todo
+
+    def delete(self, pk):
+        todo = Todo.objects.first(pk=pk)
+        todo.remove()
+        return "Todo removed"
+"""
--- ToDo/views.py
+++ ToDo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#from .models import Todo
+
+def index(request):
+    return HttpResponse("Todo app will be ready soon")
