bae8a9806077a901f9fd6f11558f3af426d68a9d
jordanrob709 <jordanrob709@gmail.com>
2020-01-15 18:03:41 +0300

    change in views to access details through description

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('details', views.detail, name='detail'),
+    path('details/<todo_description>', views.detail, name='detail'),
     path('post', views.todopost, name='post')
 ]
--- ToDo/views.py
+++ ToDo/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 #from .models import Todo
 
+
 def index(request):
     return HttpResponse("Todo app will be ready soon")
 
-def detail(request, todo_id):
-    return HttpResponse("Details of Todo {}".format(todo_id))
+
+def detail(request, todo_description):
+    return HttpResponse("Details of Todo {}".format(todo_description))
+
 
 def todopost(request):
     return HttpResponse("Post Todo")
