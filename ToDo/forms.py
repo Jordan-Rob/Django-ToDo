82114d059a419dc4e58c39965326b93f3e5fbc38
jordanrob709 <jordanrob709@gmail.com>
2020-02-07 18:09:40 +0300

    styling list template

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -3,6 +3,11 @@ from .models import Todo
 
 
 class TodoForm(forms.ModelForm):
+    name = forms.CharField()
+    description = forms.CharField()
+    Todo_date = forms.DateTimeField()
+    pub_date = forms.DateTimeField()
+
     class Meta:
         model = Todo
         fields = [
