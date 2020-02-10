af32fae7fb7e53d787c4f2fa919fbfa5fba61fec
jordanrob709 <jordanrob709@gmail.com>
2020-02-10 13:11:30 +0300

    styling forms

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -3,34 +3,34 @@ from .models import Todo
 
 
 class TodoForm(forms.ModelForm):
-    name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': ' rounded-lg mb-6 bg-gray-400 ml-12 px-2 py-2 focus:outline-none'
-            }
-        )
-    )
-    description = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': ' rounded-lg mb-6 bg-gray-400 ml-2 px-2 pt-4 pb-6 focus:outline-none'
-            }
-        )
-    )
-    Todo_date = forms.DateTimeField(
-        widget=forms.TextInput(
-            attrs={
-                'class': ' rounded-lg mb-6 bg-gray-400 ml-4 px-2 py-2 focus:outline-none'
-            }
-        )
-    )
-    pub_date = forms.DateTimeField(
-        widget=forms.TextInput(
-            attrs={
-                'class': ' rounded-lg mb-6 bg-gray-400 ml-6 px-2 py-2 focus:outline-none'
-            }
-        )
-    )
+    name = forms.CharField(label_suffix='',
+                           widget=forms.TextInput(
+                               attrs={
+                                   'class': ' rounded-lg mb-6 bg-gray-400 ml-12 px-2 py-2 focus:outline-none'
+                               }
+                           )
+                           )
+    description = forms.CharField(label_suffix='',
+                                  widget=forms.TextInput(
+                                      attrs={
+                                          'class': ' rounded-lg mb-6 bg-gray-400 ml-2 px-2 pt-4 pb-6 focus:outline-none'
+                                      }
+                                  )
+                                  )
+    Todo_date = forms.DateTimeField(label_suffix='',
+                                    widget=forms.TextInput(
+                                        attrs={
+                                            'class': ' rounded-lg mb-6 bg-gray-400 ml-4 px-2 py-2 focus:outline-none'
+                                        }
+                                    )
+                                    )
+    pub_date = forms.DateTimeField(label_suffix='',
+                                   widget=forms.TextInput(
+                                       attrs={
+                                           'class': ' rounded-lg mb-6 bg-gray-400 ml-6 px-2 py-2 focus:outline-none'
+                                       }
+                                   )
+                                   )
 
     class Meta:
         model = Todo
