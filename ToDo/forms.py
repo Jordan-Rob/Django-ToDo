76cba726b430929dbf02c69797dbdb4687cb9e20
jordanrob709 <jordanrob709@gmail.com>
2020-02-10 14:46:08 +0300

    styling form

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -6,28 +6,28 @@ class TodoForm(forms.ModelForm):
     name = forms.CharField(label_suffix='',
                            widget=forms.TextInput(
                                attrs={
-                                   'class': ' rounded-lg mb-6 bg-gray-400 ml-12 px-2 py-2 focus:outline-none'
+                                   'class': ' rounded-lg mb-6 bg-gray-400 ml-12 px-2 py-2 focus:outline-none focus:bg-gray-100  focus:border-purple-600 '
                                }
                            )
                            )
     description = forms.CharField(label_suffix='',
                                   widget=forms.TextInput(
                                       attrs={
-                                          'class': ' rounded-lg mb-6 bg-gray-400 ml-2 px-2 pt-4 pb-6 focus:outline-none'
+                                          'class': ' rounded-lg mb-6 bg-gray-400 ml-2 px-2 pt-4 pb-6 focus:outline-none focus:bg-gray-100  focus:border-purple-600 '
                                       }
                                   )
                                   )
     Todo_date = forms.DateTimeField(label_suffix='',
                                     widget=forms.TextInput(
                                         attrs={
-                                            'class': ' rounded-lg mb-6 bg-gray-400 ml-4 px-2 py-2 focus:outline-none'
+                                            'class': ' rounded-lg mb-6 bg-gray-400 ml-4 px-2 py-2 focus:outline-none focus:bg-gray-100  focus:border-purple-600 '
                                         }
                                     )
                                     )
     pub_date = forms.DateTimeField(label_suffix='',
                                    widget=forms.TextInput(
                                        attrs={
-                                           'class': ' rounded-lg mb-6 bg-gray-400 ml-6 px-2 py-2 focus:outline-none'
+                                           'class': ' rounded-lg mb-6 bg-gray-400 ml-6 px-2 py-2 focus:outline-none focus:bg-gray-100  focus:border-purple-600 '
                                        }
                                    )
                                    )
