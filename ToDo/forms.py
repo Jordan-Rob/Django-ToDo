83d8ff0d587afd72aa15814f18d9f6de3c5535da
jordanrob709 <jordanrob709@gmail.com>
2020-02-10 12:53:50 +0300

    styling form

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -6,13 +6,31 @@ class TodoForm(forms.ModelForm):
     name = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'class': ' rounded-lg mb-4 bg-gray-400 ml-4 px-2 py-2'
+                'class': ' rounded-lg mb-6 bg-gray-400 ml-12 px-2 py-2'
             }
         )
     )
-    description = forms.CharField()
-    Todo_date = forms.DateTimeField()
-    pub_date = forms.DateTimeField()
+    description = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': ' rounded-lg mb-6 bg-gray-400 ml-2 px-2 pt-4 pb-6'
+            }
+        )
+    )
+    Todo_date = forms.DateTimeField(
+        widget=forms.TextInput(
+            attrs={
+                'class': ' rounded-lg mb-6 bg-gray-400 ml-4 px-2 py-2'
+            }
+        )
+    )
+    pub_date = forms.DateTimeField(
+        widget=forms.TextInput(
+            attrs={
+                'class': ' rounded-lg mb-6 bg-gray-400 ml-6 px-2 py-2'
+            }
+        )
+    )
 
     class Meta:
         model = Todo
